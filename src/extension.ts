ebd371c7ea148d199ff746b2b1f43c4c782303cb
Chanyoung Park <rhdwkrxla68@gmail.com>
2024-10-27 01:18:01 +0900

    Feat: Create 'Create React Hook' menu

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,6 @@
 import * as vscode from 'vscode';
-import { createReactComponent } from './commands/CreateReactComponent.command';
+import { commandsFactory } from './factory';
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand('vscode-react-developer-toolkit.createReactComponent', (uri: vscode.Uri) => {
-    if (uri && uri.fsPath) {
-      vscode.window.showInputBox({ 
-        prompt: 'Enter React component name',
-        placeHolder: 'MyComponent'
-      }).then(componentName => {
-        if (componentName) {
-          createReactComponent(uri.fsPath, componentName);
-        }
-      });
-    }
-  });
-
-  context.subscriptions.push(disposable);
+  commandsFactory(context);
 }
---
+++ src/factory/ReactHook.factory.ts
@@ -0,0 +1,4 @@
+export const reactHookFactory = (hookName: string) => `export const ${hookName} = () => {
+  
+};
+`;
