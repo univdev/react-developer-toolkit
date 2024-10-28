c38c3a3f94898b68fb2870f08d9c4684aa28524e
univdev <pcydeveloper@gmail.com>
2024-10-28 14:41:40 +0900

    Remove: Removed barrel file of factory directory

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { commandsFactory } from './factory';
+import { commandsFactory } from './factory/Commands.factory';
 
 export function activate(context: vscode.ExtensionContext) {
   commandsFactory(context);
